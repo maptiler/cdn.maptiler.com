e3ea86f74042d0a8cbff12d334376e6d714bcce2
Jonathan Lurie <lurie.jo@gmail.com>
2023-03-15 11:00:28 +0100

    ADD v1.0.6 of SDK

--- maptiler-sdk-js/v1.0.5/maptiler-sdk.umd.js
+++ maptiler-sdk-js/v1.0.5/maptiler-sdk.umd.js
@@ -2248,7 +2248,7 @@
 	};
 	Object.freeze(defaults);
 
-	class MaptilerLogoControl extends maplibregl.LogoControl {
+	class CustomLogoControl extends maplibregl.LogoControl {
 	  constructor(options = {}) {
 	    var _a, _b;
 	    super(options);
@@ -2341,7 +2341,7 @@
 	  return style;
 	}
 
-	class MaptilerTerrainControl {
+	class TerrainControl$1 {
 	  constructor() {
 	    bindAll(["_toggleTerrain", "_updateTerrainIcon"], this);
 	  }
@@ -2455,7 +2455,7 @@
 	const GeolocateControl$1 = maplibregl.GeolocateControl;
 	const Marker$1 = maplibregl.Marker;
 	const LngLat$1 = maplibregl.LngLat;
-	class MaptilerGeolocateControl extends GeolocateControl$1 {
+	class CustomGeolocateControl extends GeolocateControl$1 {
 	  constructor() {
 	    super(...arguments);
 	    this.lastUpdatedCenter = new LngLat$1(0, 0);
@@ -2771,14 +2771,14 @@
 	      if ("logo" in tileJsonContent && tileJsonContent.logo) {
 	        const logoURL = tileJsonContent.logo;
 	        this.addControl(
-	          new MaptilerLogoControl({ logoURL }),
+	          new CustomLogoControl({ logoURL }),
 	          options.logoPosition
 	        );
 	        if (options.attributionControl === false) {
 	          this.addControl(new maplibregl.AttributionControl(options));
 	        }
 	      } else if (options.maptilerLogo) {
-	        this.addControl(new MaptilerLogoControl(), options.logoPosition);
+	        this.addControl(new CustomLogoControl(), options.logoPosition);
 	      }
 	      if (options.scaleControl) {
 	        const position = options.scaleControl === true || options.scaleControl === void 0 ? "bottom-right" : options.scaleControl;
@@ -2795,7 +2795,7 @@
 	      if (options.geolocateControl !== false) {
 	        const position = options.geolocateControl === true || options.geolocateControl === void 0 ? "top-right" : options.geolocateControl;
 	        this.addControl(
-	          new MaptilerGeolocateControl({
+	          new CustomGeolocateControl({
 	            positionOptions: {
 	              enableHighAccuracy: true,
 	              maximumAge: 0,
@@ -2813,7 +2813,7 @@
 	      }
 	      if (options.terrainControl) {
 	        const position = options.terrainControl === true || options.terrainControl === void 0 ? "top-right" : options.terrainControl;
-	        this.addControl(new MaptilerTerrainControl(), position);
+	        this.addControl(new TerrainControl$1(), position);
 	      }
 	      if (options.fullscreenControl) {
 	        const position = options.fullscreenControl === true || options.fullscreenControl === void 0 ? "top-right" : options.fullscreenControl;
@@ -3269,9 +3269,6 @@
 	exports.Map = Map$1;
 	exports.MapStyle = MapStyle;
 	exports.MapStyleVariant = MapStyleVariant;
-	exports.MaptilerGeolocateControl = MaptilerGeolocateControl;
-	exports.MaptilerLogoControl = MaptilerLogoControl;
-	exports.MaptilerTerrainControl = MaptilerTerrainControl;
 	exports.Marker = Marker;
 	exports.MercatorCoordinate = MercatorCoordinate;
 	exports.NavigationControl = NavigationControl;
