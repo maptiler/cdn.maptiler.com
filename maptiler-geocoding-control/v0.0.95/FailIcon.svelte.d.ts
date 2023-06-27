d783b000612d9b185991da04d13a26c88fb1440b
Martin Ždila <m.zdila@gmail.com>
2023-06-27 15:16:00 +0200

    Add maptiler-geocoding-control v0.0.95

---
+++ maptiler-geocoding-control/v0.0.95/FailIcon.svelte.d.ts
@@ -0,0 +1,23 @@
+/** @typedef {typeof __propDef.props}  FailIconProps */
+/** @typedef {typeof __propDef.events}  FailIconEvents */
+/** @typedef {typeof __propDef.slots}  FailIconSlots */
+export default class FailIcon extends SvelteComponentTyped<{
+    [x: string]: never;
+}, {
+    [evt: string]: CustomEvent<any>;
+}, {}> {
+}
+export type FailIconProps = typeof __propDef.props;
+export type FailIconEvents = typeof __propDef.events;
+export type FailIconSlots = typeof __propDef.slots;
+import { SvelteComponentTyped } from "svelte";
+declare const __propDef: {
+    props: {
+        [x: string]: never;
+    };
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {};
+};
+export {};
